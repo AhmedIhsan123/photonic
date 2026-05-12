d16b4b62c9d4c5f220df1f86150239fbd48d3b4f
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-05-12 10:50:53 -0700

    Added basic footer and header components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 import { useApod } from "./hooks/useApod";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 // import "./App.css";
 
 export default function App() {
 	const { data, loading, error } = useApod();
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error: {error}</p>;
-	if (data) return <img src={data.url} alt={data.title} />;
+	return (
+		<>
+			<Header />
+			{loading && <p>Loading...</p>}
+			{error && <p>Error: {error}</p>}
+			{data && <img src={data.url} alt={data.title} />}
+			<Footer />
+		</>
+	);
 }
