f7b2ba761ecb420f5c1d736777f7dadf04243387
Elvin Hrytsyuk <hrytsyukelvin@gmail.com>
2026-05-12 20:46:57 -0700

    header Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import "../styles/Header.css";
+
 export default function Header() {
 	return (
-		<header>
-			<h1>Header</h1>
+		<header className="site-header">
+			<div className="header-inner">
+				<span className="header-logo">✦</span>
+				<div className="header-text">
+					<h1 className="header-title">Photonic</h1>
+					<p className="header-sub">NASA Astronomy Picture of the Day</p>
+				</div>
+			</div>
 		</header>
 	);
 }
